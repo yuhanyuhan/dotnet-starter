6ebadfdb217dd74bac1b9dfa3e7f3866a0d27fbf
yuhanyuhan <33086695+yuhanyuhan@users.noreply.github.com>
2023-12-21 16:07:22 +0800

    Initial commit

---
+++ src/Boilerplate.Api/Configurations/PersistenceSetup.cs
@@ -0,0 +1,25 @@
+﻿using Boilerplate.Application.Auth;
+using Boilerplate.Domain.Auth.Interfaces;
+using Boilerplate.Infrastructure;
+using EntityFramework.Exceptions.PostgreSQL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Boilerplate.Api.Configurations;
+
+public static class PersistenceSetup
+{
+    public static IServiceCollection AddPersistenceSetup(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddScoped<ISession, Session>();
+        services.AddDbContextPool<ApplicationDbContext>(o =>
+        {
+            o.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            o.UseExceptionProcessor();
+        });
+
+        return services;
+    }
+}
